31c81065d99ff9d4b7070bed646a7e20e5189f83
Jack Gilcrest <me@jp4g.llc>
2023-09-28 03:15:22 -0600

    swap noir circuit for circom circuit

--- scripts/compile-circuit.sh
+++ scripts/compile-circuit.sh
@@ -1,14 +1,75 @@
 #!/bin/bash
 
 GREEN='\033[0;32m'
+CYAN='\033[0;36m'
 NC='\033[0m'
 
-# Compile circuit
-echo -e "${GREEN}compile-circuit.sh ${NC}| Compiling circuit..."
-cd circuits
-nargo compile &> /dev/null
+echo -e "${GREEN}compile-circuit.sh${NC}| compiling r1cs proving and verifying artifacts for the_word circom circuit, wait ~ 30 seconds..."
 
-# Move circuit to src
-mv ./target/circuits.json ../src/circuit.json
-cd ..
-echo -e "${GREEN}compile-circuit.sh ${NC}| Circuit compiled to $(pwd)/src/circuit.json"
+# if circuits/zkey does not exist, make folder
+[ -d circuits/zkey ] || mkdir circuits/zkey
+
+# compile circuit
+circom circuits/the_word.circom -o circuits/artifacts --r1cs --wasm &> /dev/null
+
+# Setup
+yarn snarkjs groth16 setup \
+    circuits/artifacts/the_word.r1cs \
+    circuits/artifacts/pot10_final.ptau \
+    circuits/artifacts/the_word.zkey &> /dev/null
+
+# Reference zkey
+yarn snarkjs groth16 setup \
+    circuits/artifacts/the_word.r1cs \
+    circuits/artifacts/pot10_final.ptau \
+    circuits/zkey/the_word_0000.zkey \
+    &> /dev/null
+
+# Circuit-specific contributions
+yarn snarkjs zkey contribute \
+    circuits/zkey/the_word_0000.zkey \
+     circuits/zkey/the_word_0001.zkey \
+    --name="First the_word contribution" -v -e="$(head -n 4096 /dev/urandom | openssl sha1)" \
+    &> /dev/null
+
+yarn snarkjs zkey contribute \
+    circuits/zkey/the_word_0001.zkey \
+    circuits/zkey/the_word_0002.zkey \
+    --name="Second the_word contribution" -v -e="$(head -n 4096 /dev/urandom | openssl sha1)" \
+    &> /dev/null
+
+yarn snarkjs zkey contribute \
+    circuits/zkey/the_word_0002.zkey \
+    circuits/zkey/the_word_0003.zkey \
+    --name="Third the_word contribution" -v -e="$(head -n 4096 /dev/urandom | openssl sha1)" \
+    &> /dev/null
+
+# Verify zkey
+yarn snarkjs zkey verify \
+    circuits/artifacts/the_word.r1cs \
+    circuits/artifacts/pot10_final.ptau \
+    circuits/zkey/the_word_0003.zkey \
+    &> /dev/null
+
+# Apply random beacon
+yarn snarkjs zkey beacon circuits/zkey/the_word_0003.zkey \
+    circuits/artifacts/the_word.zkey \
+    0102030405060708090a0b0c0d0e0f101112131415161718191a1b1c1d1e1f 10 -n="the_word FinalBeacon phase2" \
+    &> /dev/null
+
+# Verify final zkey
+yarn snarkjs zkey verify \
+    circuits/artifacts/the_word.r1cs \
+    circuits/artifacts/pot10_final.ptau \
+    circuits/artifacts/the_word.zkey \
+    &> /dev/null
+
+# Export zkey to json
+yarn snarkjs zkey export verificationkey \
+    circuits/artifacts/the_word.zkey \
+    circuits/artifacts/verifier.json \
+    &> /dev/null
+
+echo -e "${GREEN}compile-circuit.sh${NC}| successfully built proving/ verifying artifacts for the_word!"
+echo -e "${GREEN}compile-circuit.sh${NC}| artifacts located at ${CYAN}$(pwd)/circuits/artifacts${NC}"
+
