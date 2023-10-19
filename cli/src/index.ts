34594505319282c67e9b44e49cf34717d6225bfe
Jack Gilcrest <me@jp4g.llc>
2023-10-18 20:53:13 -0400

    fix wasm in npm

--- cli/src/index.ts
+++ cli/src/index.ts
@@ -1,6 +1,5 @@
 import { CircomEngine, CircuitInput, Groth16Proof } from "./utils/circom.js";
 import { checkProofPathDir, checkProofPath, getProof } from "./utils/fs.js";
-import { getWasm } from "./utils/artifacts.js";
 
 const API_URL = "https://theword.mach34.space";
 
@@ -11,6 +10,5 @@ export {
     checkProofPathDir,
     checkProofPath,
     getProof,
-    getWasm,
     API_URL,
 };
